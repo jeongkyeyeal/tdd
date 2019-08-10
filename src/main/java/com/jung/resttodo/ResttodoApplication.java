847b04ac9580c2942f13181d06dcff4cb1ae7c54
jeongkyeyeal <wjdrlduf12@gmail.com>
2019-08-10 15:39:09 +0900

    Added 프로젝트 초기 생성
    - 스프링부트를 사용해서 REST-API를 활용할 수 있도록 구성하였습니다.

---
+++ src/main/java/com/jung/resttodo/ResttodoApplication.java
@@ -0,0 +1,13 @@
+package com.jung.resttodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResttodoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResttodoApplication.class, args);
+    }
+
+}
